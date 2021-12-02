afbb2b27ec10572281fa19f067f335cf0e3df9d9
Hani Al Mashkawi <Hani.AlMashkawi@480hz.de>
2021-12-02 14:28:51 +0100

    update2

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
+const fetch = require("node-fetch");
 
-var apiRouter = require("./routes/api");
-var api2Router = require("./routes/api2");
-
+// var axios = require("axios");
+var qs = require("qs");
 var app = express();
 app.use(cors());
 app.options("*", cors());
@@ -16,13 +16,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("trust proxy", process.env.PROXY);
-app.use("/api/register", apiRouter);
-app.use("/api2/register", api2Router);
-app.use("/info", function (req, res) {
-  var fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
-  res.json({
-    fullUrl,
-  });
-});
+app.post("/", (req, res) => {
+  console.log("post");
+  const formBody = Object.keys(req.body)
+    .map(
+      (key) => encodeURIComponent(key) + "=" + encodeURIComponent(req.body[key])
+    )
+    .join("&");
 
+  fetch("https://ideenschmiede.telekom-dienste.de/api/v4/auth/register", {
+    method: "POST",
+    body: formBody,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((body) => {
+      res.json({ body: JSON.stringify(body) });
+    })
+    .catch((err) => res.json({ body: "" }));
+  // var config = {
+  //   method: "post",
+  //   url: "https://ideenschmiede.telekom-dienste.de/api/v4/auth/register",
+  //   headers: {
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //   },
+  //   data: qs.stringify({ ...req.body }),
+  // };
+  // try {
+  //   axios(config)
+  //     .then(function (response) {
+  //       console.log(JSON.stringify(response.data));
+  //       res.json({ body: response });
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //       res.json({ body: error });
+  //     });
+  // } catch (error) {
+  //   res.json({ body: error });
+  // }
+});
+app.get("/", (req, res) => {
+  console.log("get");
+  res.json({ body: { ok: true, method: "get" } });
+});
 module.exports = app;
